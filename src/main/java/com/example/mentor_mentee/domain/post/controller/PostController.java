b653d22e585c6d3c2f4993e765c8b33c41ec5694
WAcAW9 <wacaw9@gmail.com>
2025-10-14 20:16:48 +0900

    [4주차] API 시작해보기

--- src/main/java/com/example/mentor_mentee/domain/post/controller/PostController.java
+++ src/main/java/com/example/mentor_mentee/domain/post/controller/PostController.java
@@ -1,15 +1,20 @@
 package com.example.mentor_mentee.domain.post.controller;
-
-import com.example.mentor_mentee.domain.post.entity.Post;
+import com.example.mentor_mentee.domain.post.dto.request.PostRequestDto;
+import com.example.mentor_mentee.domain.post.dto.response.PostResponseDto;
+import com.example.mentor_mentee.domain.post.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+    private final PostService postService ;
 
-    @PostMapping("/")
-    public String createPost(String title, String content) {
-        return title+": "+content+"게시글 생성 완료";
+    @PostMapping
+    public PostResponseDto createPost(@RequestBody PostRequestDto postRequestDto) {
+        PostResponseDto responseDto = postService.createPost(postRequestDto);
+        return responseDto;
     }
 
     @GetMapping
@@ -18,17 +23,18 @@ public class PostController {
     }
 
     @GetMapping("/{post-id}")
-    public String getPostById(@PathVariable(value = "post-id")Long id) {
-        return id+"번 게시글 조회 완료";
+    public String getPostById(@PathVariable(value = "post-id") Long id) {
+        return id + "번 게시글 조회 완료";
     }
 
     @PutMapping("/{post-id}")
-    public String updatePost(@PathVariable(value = "post-id")Long id) {
-        return id+"번 게시글 수정 완료";
+    public String updatePost(@PathVariable(value = "post-id") Long id) {
+        return id + "번 게시글 수정 완료";
     }
 
     @DeleteMapping("/{post-id}")
-    public String deletePost(@PathVariable(value = "post-id")Long id) {
-        return id+"번 게시글 삭제 완료";
+    public String deletePost(@PathVariable(value = "post-id") Long id){
+        return id + "번 게시글 삭제 완료";
     }
+
 }
